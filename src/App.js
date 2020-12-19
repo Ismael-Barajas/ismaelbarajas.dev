baa5d38a6a63c92143a9bd38d6db7b1f73f33d40
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2020-12-18 16:04:02 -0800

    Cleaning up some stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React seeing how this updates
-        </a>
-      </header>
-    </div>
-  );
+  const underdevelopment = true;
+
+  if (underdevelopment) {
+    return <div>underconstruction buddy!</div>;
+  }
+  return <div>memer</div>;
 }
 
 export default App;
