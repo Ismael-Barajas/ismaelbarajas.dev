2e7a5c8c58418f3089cb57b554c565314d2eec54
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-05-10 21:06:00 -0700

    Updated links

--- src/Components/DevelopmentPage/Development.js
+++ src/Components/DevelopmentPage/Development.js
@@ -1,4 +1,10 @@
-import { Twitter, Instagram, GitHub } from "@material-ui/icons";
+import {
+  Twitter,
+  Instagram,
+  GitHub,
+  LinkedIn,
+  Email as EmailIcon,
+} from "@material-ui/icons";
 import { IconButton } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import "./Development.css";
@@ -32,6 +38,21 @@ export default function Development() {
           </Typing>
         </h1>
         <div className={classes.root}>
+          <a href="https://www.linkedin.com/in/ismael-barajas-52a877211/">
+            <IconButton>
+              <LinkedIn style={{ fontSize: 50 }} />
+            </IconButton>
+          </a>
+          <a href="https://github.com/Ismael-Barajas">
+            <IconButton>
+              <GitHub style={{ fontSize: 50 }} />
+            </IconButton>
+          </a>
+          <a href="mailto:ismaelbarajas.dev@gmail.com">
+            <IconButton>
+              <EmailIcon style={{ fontSize: 50 }} />
+            </IconButton>
+          </a>
           <a href="https://twitter.com/InXanee">
             <IconButton>
               <Twitter style={{ fontSize: 50 }} />
@@ -40,11 +61,6 @@ export default function Development() {
           <a href="https://www.instagram.com/lnxanee/">
             <IconButton>
               <Instagram style={{ fontSize: 50 }} />
-            </IconButton>
-          </a>
-          <a href="https://github.com/Ismael-Barajas">
-            <IconButton>
-              <GitHub style={{ fontSize: 50 }} />
             </IconButton>
           </a>
         </div>
