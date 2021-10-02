c760b2f9421b11821ad5c15fb55a1863a78312f0
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2021-10-02 12:13:02 -0700

    AboutMe edit

--- src/Components/AboutMe/AboutMe.js
+++ src/Components/AboutMe/AboutMe.js
@@ -39,15 +39,13 @@ export default function AboutMe() {
                 <br />
                 <br />
                 As of late I've taken an interest in Front End developing with
-                React, building out projects with create-react-app (Client side
-                rendering) and next.js (Server side rendering). You can check
-                out some of the projects I have worked on below! I would say I’m
-                most proficient with React/JavaScript/JSX and C++. I’m also
-                familiar with other languages as well, such as C and Python. I
-                wouldn’t consider myself a Front End Developer just yet because
-                it’s very early in my career and interests change over time. So
-                who knows what other projects, languages, and career
-                opportunities await!
+                React. You can check out some of the projects I have worked on
+                below! I would say I’m most proficient with React/JavaScript/JSX
+                and C++. I’m also familiar with other languages as well, such as
+                C and Python. I wouldn’t consider myself a Front End Developer
+                just yet because it’s very early in my career and interests
+                change over time. So who knows what other projects, languages,
+                and career opportunities await!
                 <br />
                 <br />
                 Feel free to contact me anytime through any of the platforms
