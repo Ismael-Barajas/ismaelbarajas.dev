6ba5e82530f7c15745dcba7d8d662cecd54987ff
Ismael <54871513+Ismael-Barajas@users.noreply.github.com>
2022-01-04 00:06:28 -0800

    info snackbar added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,21 @@ import {
   Footer,
   ProgressBar,
 } from "./Components";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import React from "react";
+import { IconButton, Snackbar } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
 
 function App() {
+  const [open, setOpen] = React.useState(true);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
   return (
     <>
       <main>
@@ -17,6 +30,35 @@ function App() {
         <Projects />
         <ScrollButton />
         <Footer />
+        <Snackbar
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          open={open}
+          onClose={handleClose}
+        >
+          <Alert
+            severity="info"
+            color="info"
+            variant="filled"
+            action={
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={handleClose}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            }
+          >
+            <AlertTitle>Update</AlertTitle>
+            ismaelbarajas.dev v2 is currently being developed. —{" "}
+            <strong>
+              <a href="https://github.com/Ismael-Barajas/ismaelbarajas.dev-v2">
+                check out the progress here!
+              </a>
+            </strong>
+          </Alert>
+        </Snackbar>
       </main>
     </>
   );
